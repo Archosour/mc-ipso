214b93a2f294d43db12bb97ec637b2554f7f0442
Archosaur <marc.vd.klooster@home.nl>
2023-11-20 23:36:58 +0100

    furter cleanup

--- startup.lua
+++ startup.lua
@@ -8,27 +8,6 @@ local Tab = 0
 local Console_tab = multishell.getCurrent()
 local Pc_label = os.getComputerLabel()
 
-function Run_kinetic_info(Peripheral_side, Speed_side, Stress_side) -- obsolete
-	local Interface = peripheral.wrap(Peripheral_side)
-
-	local RPM = math.abs(Interface.getKineticSpeed(Speed_side))
-	local Direction = true
-	local Stress = Interface.getKineticStress(Stress_side)
-	local Capacity = Interface.getKineticCapacity(Stress_side)
-
-	if RPM < 0 then Direction = false end
-
-	local RPM_object 	   = IPSO.Generate_object(IPSO.Object_list.Kinetic_speed, 	  0, IPSO.Resource_list.Set_value, RPM)
-	local Direction_object = IPSO.Generate_object(IPSO.Object_list.Kinetic_direction, 0, IPSO.Resource_list.Set_value, Direction)
-	local Stress_object    = IPSO.Generate_object(IPSO.Object_list.Kinetic_stress,    0, IPSO.Resource_list.Set_value, Stress)
-	local Capacity_object  = IPSO.Generate_object(IPSO.Object_list.Kinetic_capacity,  0, IPSO.Resource_list.Set_value, Capacity)
-
-	local Package = Brave.Generate_package({RPM_object, Direction_object,Stress_object,Capacity_object}, Brave.Package_types.Broadcast, {})
-	--Brave.Log(textutils.serialise(Package), true)
-	Brave.Modem.transmit(1,1,Package)
-
-end
-
 function Startup()
 	if pocket then
 		Tab = multishell.launch({}, "Pocket.lua", {"true"})
@@ -51,6 +30,7 @@ function Startup()
 		while true do end
 	end
 	Device_type = Config.Device_type
+	print("Device type: " .. Device_type)
 
 	Brave.Modem.open(Config.Channel_network)
 
